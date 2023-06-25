a60ec629faaf3a6ba5ab44d7ba0c3b9796295b2f
joal <jokke34@gmail.com>
2023-06-25 13:14:49 +0200

    added swiper

--- src/app/app/app.module.ts
+++ src/app/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+import { SwiperModule } from 'swiper/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    SwiperModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,43 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { IonSegment } from '@ionic/angular';
+import { SwiperComponent } from 'swiper/angular';
 
 @Component({
   selector: 'home',
   templateUrl: 'home.page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  public segmentList = ['Track', 'History', 'Graph'];
+  public selectedSegment = '';
+
+  @ViewChild('segments') segments?: IonSegment;
+  @ViewChild('swiper', { static: false }) swiper?: SwiperComponent;
+
+  constructor(private changeDetector: ChangeDetectorRef) {}
+
+  ngOnInit() {
+    this.selectedSegment = this.segmentList[0];
+  }
+
+  segmentSelected(item: string, index: number) {
+    console.log(item, index);
+  }
+
+  onSwipe(event: any) {
+    console.log(event);
+  }
+
+  onSlideChange(event: any) {
+    if (!this.swiper) return;
+    const index = this.swiper.swiperRef.activeIndex;
+    this.selectedSegment = this.segmentList[index];
+    this.changeDetector.detectChanges();
+  }
 }
